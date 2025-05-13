2619562ca3ae4675957d99f2c10227de22a72d4a
jboomer007 <jboomer@brickwallkc.com>
2025-05-13 11:52:27 -0500

    Update HasQuickBooksToken.php
    Set Master User on Library

--- src/HasQuickBooksToken.php
+++ src/HasQuickBooksToken.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 trait HasQuickBooksToken
 {
+    /** Set to Master User */
+    public function getMasterIdAttribute(){
+        return 1;
+    }
     /**
      * Have a quickBooksToken.
      */
     public function quickBooksToken(): HasOne
     {
-        return $this->hasOne(Token::class);
+        return $this->hasOne(Token::class,'user_id','master_id');
     }
 }
